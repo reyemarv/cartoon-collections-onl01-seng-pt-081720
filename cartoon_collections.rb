6d0a34ef41f4b1d7f5a99c2b28f7740c40f65910
Marvin Reyes <reyemarv@gmail.com>
2021-01-31 04:31:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(names)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map{|i| i.
 end
 
 def long_planeteer_calls# code an argument here
